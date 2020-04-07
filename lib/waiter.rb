0727aacad7b709acbfba67a859ab0261be02b3bb
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2020-04-07 18:15:18 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,16 @@
 class Waiter
-
+  attr_accessor :name, :yrs_expierence 
+  
+  @@all = []
+  
+  def initialize(name, yrs_expierence)
+    @name = name 
+    @yrs_expierence = yrs_expierence
+    @@all << self 
+    
+  end 
+  
+  def self.all 
+    @@all 
+  end 
 end
